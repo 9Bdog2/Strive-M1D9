5610bc2068ce8255ff34cd5136d85cc4a1e5b4a2
Bogdan Birau <birau.bogdan92@gmail.com>
2021-08-19 15:58:28 +0100

    Trying to add clicked functionality

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ const displayEntireBoard = function(){
         newNumberNode.id = number 
         newNumberNode.classList.add("number") 
 
+        newNumberNode.onclick = selectNumber
+
         boardContainerNode.appendChild(newNumberNode)
     }
 }
 
+const selectNumber = function(eventData){
+    // DESELECT THE CURRENTLY SELECTED Number
+    let currentSelectedNumber = getSelectedNumber
+    if(currentSelectedNumber !== null){
+        currentSelectedNumber.classList.remove("selected")
+    }
+    // SELECT THE Number THAT HAS JUST BEEN CLICKED
+    let clickedNumberNode = eventData.target
+    clickedNumberNode.classList.add("selected")
+    currentSelectedNumber = clickedNumberNode
+
+}
+
+const getSelectedNumber = function(){
+    return document.querySelector(".selected")
+}
+
+const random = function(){
+    alert("Clicked")
+}
+
 window.onload = function() {
+    getSelectedNumber()
     displayEntireBoard()
 }
